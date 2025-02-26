81dc39cf017c892178ade4a0831c44f2fe3abacf
fperdigon <fperdigon88@gmail.com>
2025-02-26 18:55:18 +0000

    adding cmd arguments handling

--- easybackup_cmd.py
+++ easybackup_cmd.py
@@ -9,35 +9,58 @@
 #
 # ===========================================================
 
+import argparse
 from utils.easybackup_core import run_incremental_backup
 from utils.logger import logger  # Import the shared logger
 
+# This variable stores all the backup configurations
+BACKUP_CONFIGS = []
 
-if __name__ == "__main__":    
-    # Example Usage:
-    # incremental_backup("/local/source/", "/backup", "user", "remote.server.com", ssh_password="your_password")
-    
-    # TODO: Possible fix to avoid storing pass
-    # create key for the program
-    # ssh-keygen -t rsa -b 4096 -f ~/.ssh/easybackup_key -N ""
-    # ssh-copy-id -i ~/.ssh/easybackup_key.pub -p <ssh_port> <user>@<remote_host>
-    # or
-    # cat ~/.ssh/backup_key.pub | ssh -p <ssh_port> <user>@<remote_host> "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
-    # test connection whithout a pasword
-    # ssh -i ~/.ssh/backup_key -p <ssh_port> <user>@<remote_host>
+def main():
+    parser = argparse.ArgumentParser(description="Backup Management Script")
+    parser.add_argument("-cbc", "--create-backup", 
+                        help="Create a new backup configuration.")
+    parser.add_argument("-lbc", "--list-backups", action="store_true", 
+                        help="List all backup configurations.")
+    parser.add_argument("-rbc", "--run-backup", metavar="NAME", 
+                        help="Run a backup configuration.")
 
-    # Adding credentials stored infile for testing
+    args = parser.parse_args()
 
-    
-    # Temporal backup configuration
-    from utils.credentials_raw_testing_DO_NOT_ADD_TO_REPO import test_backup_config
-    backup_conf_dict = test_backup_config["NAS No Key"]
+    # DEBUG: Simulating run_backup entry
+    class Args:
+        run_backup = "NAS No Key"
+        create_backup = None
+        list_backups = None
+    args = Args()
 
-    logger.info(f"Starting backup using configuration named: {backup_conf_dict['name']}")
-    run_incremental_backup(src=backup_conf_dict["src"],
-                           dest=backup_conf_dict["dest"],
-                           user=backup_conf_dict["user"],
-                           ssh_password=backup_conf_dict["ssh_password"],
-                           remote_host=backup_conf_dict["remote_host"],
-                           ssh_port=backup_conf_dict["ssh_port"],
-                           keep_days=backup_conf_dict["keep_days"])
+    if args.create_backup:
+        global BACKUP_CONFIGS
+        #BACKUP_CONFIGS = create_backup_config(BACKUP_CONFIGS)
+
+    elif args.list_backups:
+        #list_backup_configs()
+        pass
+
+    elif args.run_backup:
+        #run_backup(args.run_backup)
+
+        from utils.credentials_raw_testing_DO_NOT_ADD_TO_REPO import test_backup_config
+        backup_conf_dict = test_backup_config["NAS No Key"]
+
+        logger.info(f"Starting backup using configuration named: {backup_conf_dict['name']}")
+        run_incremental_backup(local_path=backup_conf_dict["local_path"],
+                               remote_path=backup_conf_dict["remote_path"],
+                               ssh_user=backup_conf_dict["ssh_user"],
+                               ssh_password=backup_conf_dict["ssh_password"],
+                               remote_host=backup_conf_dict["remote_host"],
+                               ssh_port=backup_conf_dict["ssh_port"],
+                               keep_days=backup_conf_dict["keep_days"])
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
+
+
