27997dddf1508de59560517641dcaf96123ca01c
fperdigon <fperdigon88@gmail.com>
2025-03-04 20:20:58 +0000

    adjusting code for adding new functionalities

--- easybackup_cmd.py
+++ easybackup_cmd.py
@@ -11,37 +11,40 @@
 
 import argparse
 from utils.cmd_credentials_management import create_backup_config_cmd, \
-     list_backup_configs_cmd, del_backup_configs_cmd
-from utils.easybackup_core import run_incremental_backup
+     list_backup_configs_cmd, del_backup_configs_cmd, modify_backup_configs_cmd,\
+     run_backup, run_all_active_backups
+# from utils.easybackup_core import run_incremental_backup
 from utils.logger import logger  # Import the shared logger
-
-# This variable stores all the backup configurations
-BACKUP_CONFIGS = {}
 
 def main():
     parser = argparse.ArgumentParser(description="Backup Management Script")
-    parser.add_argument("-cbc", "--create-backup", 
+    parser.add_argument("-cbc", "--create-backup", action="store_true", 
                         help="Create a new backup configuration.")
     parser.add_argument("-lbc", "--list-backups", action="store_true", 
                         help="List all backup configurations.")
     parser.add_argument("-rbc", "--run-backup", metavar="NAME", 
                         help="Run a backup configuration."),
-    parser.add_argument("-dbc", "--del-backup", 
+    parser.add_argument("-dbc", "--del-backup", action="store_true", 
                         help="Delete a backup configuration.")
+    parser.add_argument("-mbc", "--modify-backup", action="store_true", 
+                        help="Modify a backup configuration.")                
 
     args = parser.parse_args()
 
+    ######################################
     # DEBUG: Simulating run_backup entry
-    class Args:
-        #run_backup = "NAS No Key"
-        run_backup = None
-        create_backup = None
-        # create_backup = True
-        list_backups = None
-        # list_backups = True
-        del_backup = True
+    # class Args:
+    #     #run_backup = "NAS No Key"
+    #     run_backup = None
+    #     create_backup = None
+    #     # create_backup = True
+    #     list_backups = None
+    #     # list_backups = True
+    #     del_backup = None
+    #     modify_backup = None
 
-    args = Args()
+    # args = Args()
+    ######################################
 
     if args.create_backup:
         create_backup_config_cmd()
@@ -51,21 +54,12 @@ def main():
 
     elif args.del_backup:
         del_backup_configs_cmd()
+
+    elif args.modify_backup:        
+        modify_backup_configs_cmd()        
         
-    elif args.run_backup:
-        #run_backup(args.run_backup)
-
-        from utils.credentials_raw_testing_DO_NOT_ADD_TO_REPO import test_backup_config
-        backup_conf_dict = test_backup_config["NAS No Key"]
-
-        logger.info(f"Starting backup using configuration named: {backup_conf_dict['name']}")
-        run_incremental_backup(local_path=backup_conf_dict["local_path"],
-                               remote_path=backup_conf_dict["remote_path"],
-                               ssh_user=backup_conf_dict["ssh_user"],
-                               ssh_password=backup_conf_dict["ssh_password"],
-                               remote_host=backup_conf_dict["remote_host"],
-                               ssh_port=backup_conf_dict["ssh_port"],
-                               keep_days=backup_conf_dict["keep_days"])
+    elif args.run_backup:                
+        run_backup(config_name=args.run_backup)
     else:
         parser.print_help()
 
